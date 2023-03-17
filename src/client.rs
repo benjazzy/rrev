9a178c79ba8bbcd7d0327eb55ca6fcf7f9ba135e
benjazzy <benjazzy64@gmail.com>
2023-03-17 03:31:41 -0400

    Added client connect function.

--- src/client.rs
+++ src/client.rs
@@ -1,1 +1,24 @@
+use tokio_tungstenite::tungstenite;
+use url::Url;
+use crate::connection::ConnectionHdl;
 
+pub async fn connect<
+    OurReq,
+    OurRep,
+    OurEvent,
+    TheirReq,
+    TheirRep,
+    TheirEvent
+>(url: Url) -> Result<ConnectionHdl<
+    OurReq,
+    OurRep,
+    OurEvent,
+    TheirReq,
+    TheirRep,
+    TheirEvent
+>, tungstenite::error::Error> {
+    let (ws_stream, _) = tokio_tungstenite::connect_async(url).await?;
+
+    // let hdl = ConnectionHdl::new(ws_stream);
+    todo!()
+}
