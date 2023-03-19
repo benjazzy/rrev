32c9b607d9e31c1ea6bb8939a129f1db5932bad3
benjazzy <benjazzy64@gmail.com>
2023-03-18 22:41:58 -0400

    I am stupid.

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+mod client;
+mod connection;
+mod scheme;
+mod server;
+
+pub use scheme::Requestable;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
