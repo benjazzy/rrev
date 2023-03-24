34c442c5eeb80d3c9108157fe65b3ba2de9103e8
benjazzy <benjazzy64@gmail.com>
2023-03-23 22:38:12 -0400

    Added doc to connection_passer.rs.

--- src/server/connection_passer.rs
+++ src/server/connection_passer.rs
@@ -5,6 +5,12 @@ use std::net::SocketAddr;
 use tokio::sync::mpsc;
 use tracing::warn;
 
+/// Passes events from a connection to a server with the connection address.
+///
+/// # Arguments
+/// * `rx` - Receiver to listen for connection events.
+/// * `server_hdl` - Server handle to pass connection events on to.
+/// * `addr` - Address of the connection. Used to identify the connection event to the server.
 pub async fn pass_messages<P: Parser>(
     mut rx: mpsc::Receiver<ConnectionEvent<P>>,
     server_hdl: PassersServerHandle<P>,
